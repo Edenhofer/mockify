8fa7378fd9860c732a84b2cfab631692e198ef25
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2019-06-20 13:36:07 +0200

    Format code (PRETTIER for JS and Beautify for HTML)

--- background.js
+++ background.js
@@ -1,84 +1,89 @@
-"use strict"
+"use strict";
 
-
-let TAG = "mockify"
+let TAG = "mockify";
 // TODO: Write a proper configuration handler
-let config = {}
+let config = {};
 let fallback_config = {
-	"power": true,
-	"debug_mode": true,
-	"mock_user_agent": true,
-	"block_tracking_urls": false,
-	"alt_header": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
-}
-
+	power: true,
+	debug_mode: true,
+	mock_user_agent: true,
+	block_tracking_urls: false,
+	alt_header:
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+};
 
 /*
-* Simpler function to log messages
-*/
+ * Simpler function to log messages
+ */
 function log(message) {
 	console.log(new Date() + " " + TAG.toUpperCase() + ": " + message);
 }
 
-
 /*
-* Rewrite the request header to mock the USER_AGENT
-*/
+ * Rewrite the request header to mock the USER_AGENT
+ */
 function rewriteRequestHeader(e) {
 	if (config.debug_mode) log("Modifying " + e.url);
 
 	for (let header of e.requestHeaders) {
 		if (header.name.toLowerCase() === "user-agent") {
-			header.value = config.alt_header
+			header.value = config.alt_header;
 		}
 	}
 
-	return { requestHeaders: e.requestHeaders }
+	return {
+		requestHeaders: e.requestHeaders
+	};
 }
 
-
 /*
-* Rewrite the response header to mock the USER_AGENT
-*/
+ * Rewrite the response header to mock the USER_AGENT
+ */
 function rewriteResponseHeader(e) {
 	if (config.debug_mode) log("Modifying " + e.url);
 
 	for (let header of e.responseHeaders) {
 		if (header.name.toLowerCase() === "user-agent") {
-			header.value = config.alt_header
+			header.value = config.alt_header;
 		}
 	}
 
-	return { responseHeaders: e.responseHeaders }
+	return {
+		responseHeaders: e.responseHeaders
+	};
 }
 
-
 /*
-* Add rewriteRequestHeader to onBeforeSendHeaders and rewriteResponseHeader to
-* onHeadersReceived.
-*/
+ * Add rewriteRequestHeader to onBeforeSendHeaders and rewriteResponseHeader to
+ * onHeadersReceived.
+ */
 function addHeaderListeners() {
-	let target_urls = ["<all_urls>"]
+	let target_urls = ["<all_urls>"];
 
 	/* Use blocking listeners to also modify the header */
-	browser.webRequest.onBeforeSendHeaders.addListener(rewriteRequestHeader,
-		{ urls: target_urls },
-		["blocking", "requestHeaders"]);
+	browser.webRequest.onBeforeSendHeaders.addListener(
+		rewriteRequestHeader,
+		{
+			urls: target_urls
+		},
+		["blocking", "requestHeaders"]
+	);
 
-	browser.webRequest.onHeadersReceived.addListener(rewriteResponseHeader,
-		{ urls: target_urls },
-		["blocking", "responseHeaders"]);
+	browser.webRequest.onHeadersReceived.addListener(
+		rewriteResponseHeader,
+		{
+			urls: target_urls
+		},
+		["blocking", "responseHeaders"]
+	);
 }
 
-
 /*
-* Remove rewriteRequestHeader and rewriteResponseHeader
-*/
+ * Remove rewriteRequestHeader and rewriteResponseHeader
+ */
 function removeHeaderListeners() {
-	browser.webRequest.onBeforeSendHeaders.removeListener(
-		rewriteRequestHeader);
-	browser.webRequest.onHeadersReceived.removeListener(
-		rewriteResponseHeader);
+	browser.webRequest.onBeforeSendHeaders.removeListener(rewriteRequestHeader);
+	browser.webRequest.onHeadersReceived.removeListener(rewriteResponseHeader);
 }
 
 function updateListeners() {
@@ -88,28 +93,37 @@ function updateListeners() {
 		return;
 	}
 
-	if (config.mock_user_agent) { addHeaderListeners(); }
-	else { removeHeaderListeners(); }
+	if (config.mock_user_agent) {
+		addHeaderListeners();
+	} else {
+		removeHeaderListeners();
+	}
 
-	if (config.block_tracking_urls) null;  // TODO
+	if (config.block_tracking_urls) null; // TODO
 }
 
 function handleMessage(message, sender, sendResponse) {
 	if (message.action === "reload") {
 		// Load local configuration
-		browser.storage.local.get("config").then(function (response) {
-			// We already know that the config object exist so just make it
-			// globally available
-			config = response.config;
+		browser.storage.local.get("config").then(
+			function(response) {
+				// We already know that the config object exist so just make it
+				// globally available
+				config = response.config;
 
-			if (config.debug_mode) log("Loaded the following configuration: " +
-				JSON.stringify(response));
+				if (config.debug_mode)
+					log(
+						"Loaded the following configuration: " +
+							JSON.stringify(response)
+					);
 
-			updateListeners();
-		},
+				updateListeners();
+			},
 			function onError() {
-				if (browser.runtime.lastError) console.error(
-					"Runtime Error :( " + browser.runtime.lastError);
+				if (browser.runtime.lastError)
+					console.error(
+						"Runtime Error :( " + browser.runtime.lastError
+					);
 			}
 		);
 	}
@@ -117,24 +131,26 @@ function handleMessage(message, sender, sendResponse) {
 
 addHeaderListeners();
 // Load local configuration and update listeners
-browser.storage.local.get("config").then(function (response) {
-	// Make the (fallback) configuration globally available
-	if (Object.entries(response).length === 0) {
-		config = fallback_config;
-	} else {
-		config = response.config
-	}
+browser.storage.local.get("config").then(
+	function(response) {
+		// Make the (fallback) configuration globally available
+		if (Object.entries(response).length === 0) {
+			config = fallback_config;
+		} else {
+			config = response.config;
+		}
 
-	if (config.debug_mode) {
-		log("Loaded the following configuration: " +
-			JSON.stringify(config));
-	}
+		if (config.debug_mode) {
+			log(
+				"Loaded the following configuration: " + JSON.stringify(config)
+			);
+		}
 
-	updateListeners();
-},
+		updateListeners();
+	},
 	function onError() {
-		if (browser.runtime.lastError) console.error("Runtime Error :( " +
-			browser.runtime.lastError);
+		if (browser.runtime.lastError)
+			console.error("Runtime Error :( " + browser.runtime.lastError);
 	}
 );
 // Add a listener for communicating with the settings page
--- popup/config.js
+++ popup/config.js
@@ -1,71 +1,79 @@
-"use strict"
+"use strict";
 
-
-let TAG = "mockify"
-let binary_settings = ["power", "debug_mode", "mock_user_agent",
-	"block_tracking_urls"]
+let TAG = "mockify";
+let binary_settings = [
+	"power",
+	"debug_mode",
+	"mock_user_agent",
+	"block_tracking_urls"
+];
 let fallback_config = {
-	"power": true,
-	"debug_mode": true,
-	"mock_user_agent": true,
-	"block_tracking_urls": false,
-	"alt_header": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
-}
+	power: true,
+	debug_mode: true,
+	mock_user_agent: true,
+	block_tracking_urls: false,
+	alt_header:
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+};
 
-
-window.onload = function () {
+window.onload = function() {
 	initConfigurationPage();
 
 	// Add event listeners for every possible value that could be changed
 	for (let setting of binary_settings) {
-		document.getElementById("toggle_" + setting).addEventListener("click",
-			updateConfiguration);
+		document
+			.getElementById("toggle_" + setting)
+			.addEventListener("click", updateConfiguration);
 	}
-}
+};
 
 /*
-* Simpler function to log messages
-*/
+ * Simpler function to log messages
+ */
 function log(message) {
 	console.log(new Date() + " " + TAG.toUpperCase() + ": " + message);
 }
 
 function initConfigurationPage() {
 	// Load local configuration
-	browser.storage.local.get("config").then(function (response) {
-		let config = {};
-		if (Object.entries(response).length === 0) {
-			config = fallback_config;
-		} else {
-			config = response.config
-		}
-
-		if (config.debug_mode) {
-			log("Loaded the following configuration: " +
-				JSON.stringify(config));
-		}
-
-		// Adapt the page to the currently configured values
-		for (let setting of binary_settings) {
-			if (config[setting]) {
-				document.getElementById("toggle_" + setting).checked = true;
+	browser.storage.local.get("config").then(
+		function(response) {
+			let config = {};
+			if (Object.entries(response).length === 0) {
+				config = fallback_config;
+			} else {
+				config = response.config;
 			}
-		}
-	},
+
+			if (config.debug_mode) {
+				log(
+					"Loaded the following configuration: " +
+						JSON.stringify(config)
+				);
+			}
+
+			// Adapt the page to the currently configured values
+			for (let setting of binary_settings) {
+				if (config[setting]) {
+					document.getElementById("toggle_" + setting).checked = true;
+				}
+			}
+		},
 		function onError() {
-			if (browser.runtime.lastError) console.error("Runtime Error :( " +
-				browser.runtime.lastError);
+			if (browser.runtime.lastError)
+				console.error("Runtime Error :( " + browser.runtime.lastError);
 		}
 	);
 }
 
 function updateConfiguration() {
-	let config = {}
-	config.alt_header = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+	let config = {};
+	config.alt_header =
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
 
 	// Get all setting values
 	for (let setting of binary_settings) {
-		config[setting] = document.getElementById("toggle_" + setting).checked
+		config[setting] = document.getElementById("toggle_" + setting).checked;
 	}
 
 	// Actually save the settings
@@ -77,10 +85,13 @@ function updateConfiguration() {
 	}
 
 	// Send a message to the background process to apply the settings
-	browser.runtime.sendMessage({ action: "reload"}).then(function (m) {
-		if (config.debug_mode) log("Message from the background script: " +
-			JSON.stringify(m));
-	},
-		function (e) { console.error(e); }
+	browser.runtime.sendMessage({ action: "reload" }).then(
+		function(m) {
+			if (config.debug_mode)
+				log("Message from the background script: " + JSON.stringify(m));
+		},
+		function(e) {
+			console.error(e);
+		}
 	);
 }
